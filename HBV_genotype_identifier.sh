e678e5bf6cf1a6f95e73e1e9a2324375bb238a03
Yuan Olivia ZHU <30611503+YuanOZhu@users.noreply.github.com>
2018-08-07 10:47:21 +0800

    Update HBV_genotype_identifier.sh

--- HBV_genotype_identifier.sh
+++ HBV_genotype_identifier.sh
@@ -3,12 +3,13 @@
 ##Author: ZHU O. Yuan
 ##Script name: HBV_genotype_identifier.sh
 ##Hepatitis B genotype identifier script called upon by HBVmap.sh
-##Usage: HBV_genotype_identifier.sh PATH_TO_REFERENCE
+##Usage: HBV_genotype_identifier.sh $PATH_TO_REFERENCE $NUMBER_LINES
 ##Calls: bwa
 ##Outputs text summary file genotype_identifier.verbose.txt
 
-REF=$1
-FILE="genotype_identifier.verbose.txt" 
+REF=$1 #location of reference fasta
+LINES=$2 #number of fastq lines to use
+FILE="genotype_identifier.verbose.txt" #standard output file
 #makes sure no pre-existing output file exists
 if [ -f $FILE ]; then
     echo "File $FILE exists. Please verify before proceeding."
@@ -20,5 +21,5 @@ fi
 #25000 is arbitrary and was sufficient to identify best match genotype for 151bp reads. modify if necessary
 for filename in ./*R1*fastq.gz; do
     echo $filename >> genotype_identifier.verbose.txt
-    gunzip -c $filename | tail -25000 | bwa mem $REF - | awk '{print $3}' | sort | uniq -c | grep -v ':' | awk '{print $1"\t"$2}' | sort -r -k1,1n >> genotype_identifier.verbose.txt
+    gunzip -c $filename | tail -$2 | bwa mem $REF - | awk '{print $3}' | sort | uniq -c | grep -v ':' | awk '{print $1"\t"$2}' | sort -r -k1,1n >> genotype_identifier.verbose.txt
 done

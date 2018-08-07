0b76dc3813949ff99001b958a333e5ceb592928e
Yuan Olivia ZHU <30611503+YuanOZhu@users.noreply.github.com>
2018-08-07 11:04:15 +0800

    Update HBVmap.sh

--- HBVmap.sh
+++ HBVmap.sh
@@ -1,35 +1,35 @@
 #!/bin/bash
 
-##qsub -pe OpenMP 8 -l mem_free=128G,h_rt=72:00:00 -cwd -V -b y
-#qsub -pe OpenMP 8 -l mem_free=16G,h_rt=48:00:00 -cwd -V -b y HBVmap.sh 0 map
-#qsub -pe OpenMP 8 -l mem_free=16G,h_rt=48:00:00 -cwd -V -b y HBVmap.sh match map
-##remember to clear all files
+##Author: ZHU O. Yuan
+##Script name: HBVmap.sh
+##Hepatitis B mapper wrapper script
+##Usage: mapHBV.sh match $PATH/ref.fa 25000 map
+##Calls: bwa lofreq R
+##Requires: HBV_Genotype8.fa (contains all 8 HBV genotypes, prepare for use as reference prior to running this script)
+
 initial_wd=`pwd`
 
-if [[ $# -eq 0 ]] ;
+if [[ $# -eq 0 ]] ; #print help message to screen if no options were provided
 then
-    echo 'Usage: mapHBV.sh match map'
+    echo 'Usage: mapHBV.sh match REF LINES map'
     echo 'match :Identify best match genotype if genotype_caller.out file is not already present. type anything ele to skip this'
     echo 'map :Map files according to best match genome as identified. use anything else to skip' 
     echo 'Running script from '$initial_wd
     echo 'Make sure you are running this from the fastq directory with paired fastq files'
+    echo 'Can be run on multiple samples in the same folder'
     exit 0
 else
-    
-    if [[ $1 = 'match' ]];
+    if [[ $1 = 'match' ]]; #if option 'match' given, launch genotype calling scripts
     then
 	echo 'Identifying best match genotypes'	
-	HBV_genotype_identifier.sh
-	cp /home/zhuy/bin/HBV_genotype_caller.pl ./
-	perl HBV_genotype_caller.pl genotype_identifier.verbose.txt 25000 > genotype_caller.out
+	HBV_genotype_identifier.sh $2 $3 #script to report estimated %identity to each of 8 genotypes provided
+	perl HBV_genotype_caller.pl genotype_identifier.verbose.txt $3 > genotype_caller.out #identifies closest genotype
     else
 	echo 'Best match genotype calling skipped'
     fi
-    
-    if [[ $2 = 'map' ]];
+    if [[ $4 = 'map' ]];
     then
 	echo 'Mapping to best match genotypes'
-	cp /home/zhuy/bin/HBV_genotype_mapper.pl ./
-	perl /home/zhuy/bin/HBV_genotype_mapper.pl genotype_caller.out
+	perl HBV_genotype_mapper.pl genotype_caller.out #maps sample to best match reference
     fi
 fi

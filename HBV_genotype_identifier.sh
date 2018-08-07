3dcaf4318045c4e14e944c4a6d94ebaf067b9ae5
Yuan Olivia ZHU <30611503+YuanOZhu@users.noreply.github.com>
2018-08-07 10:31:38 +0800

    Update HBV_genotype_identifier.sh

--- HBV_genotype_identifier.sh
+++ HBV_genotype_identifier.sh
@@ -1,21 +1,24 @@
 #!/bin/bash
 
-REF="/mnt/projects/zhuy/ecoli_pop_gen/00_All_references/HBV_Reference_non-ambiguous/Genotype8.fasta"
+##Author: ZHU O. Yuan
+##Script name: HBV_genotype_identifier.sh
+##Hepatitis B genotype identifier script called upon by HBVmap.sh
+##Usage: HBV_genotype_identifier.sh PATH_TO_REFERENCE
+##Calls: bwa
+##Outputs text summary file genotype_identifier.verbose.txt
 
-FILE="genotype_identifier.verbose.txt"
+REF=$1
+FILE="genotype_identifier.verbose.txt" 
+#makes sure no pre-existing output file exists
 if [ -f $FILE ]; then
     echo "File $FILE exists. Please verify before proceeding."
     exit 1
 else
     echo "File $FILE does not exist. Proceed."
 fi
-#rm genotype_identifier.verbose.txt
-
+#for each R1 fastq file, map 25000/4 reads to HBV_Genotype8.fa and summarize number of reads mapped to each
+#25000 is arbitrary and was sufficient to identify best match genotype for 151bp reads. modify if necessary
 for filename in ./*R1*fastq.gz; do
-    #echo $filename >> genotype_identifier.fast.txt
     echo $filename >> genotype_identifier.verbose.txt
-    #gunzip -c $filename | tail -10000 | bwa mem $REF - | awk '{print $3}' | sort | uniq -c | awk '{print $1"\t"$2}' | grep 'ref' | sort -k1,1n | tail -1 >> genotype_identifier.fast.txt
     gunzip -c $filename | tail -25000 | bwa mem $REF - | awk '{print $3}' | sort | uniq -c | grep -v ':' | awk '{print $1"\t"$2}' | sort -r -k1,1n >> genotype_identifier.verbose.txt
 done
-
-#perl HBV_genotype_caller.pl genotype_identifier.verbose.txt 25000 > genotype_caller.out

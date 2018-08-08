518ec0beeab46cdf4e1a27c939bb94173371b034
Yuan Olivia ZHU <30611503+YuanOZhu@users.noreply.github.com>
2018-08-08 17:43:48 +0800

    Update HBV_genotype_coverage.R

--- HBV_genotype_coverage.R
+++ HBV_genotype_coverage.R
@@ -1,22 +1,25 @@
-#Rscript Plot_coverage.R AHB144_B.sorted.grouped.realign.coverage AHB144_B.sorted.grouped.realign.coverage.sample_summary 
-#../04_Sample_vcfs/AHB152_B.sorted.grouped.realign.recal.lofreq.vcf
+##Author: ZHU O. Yuan 2017
+##Script name: HBV_genotype_coverage.R
+##Rscript to generate coverage plot and SNP locations
+##Usage: Rscript HBV_genotype_coverage.R $ID $MEAN $COV $VCF $Rout
 
 args<-commandArgs(TRUE)
-#write(args[],stdout())
-id<-args[1]
-aveCov<-args[2]
+id<-args[1] #sample name
+aveCov<-args[2] #sample mean coverage
 
-pdf(args[5])
+pdf(args[5]) #pdf file name
 
-data<-read.table(file=args[3],sep="\t",header=TRUE)
+data<-read.table(file=args[3],sep="\t",header=TRUE) #coverage file
 data_pos<-do.call(rbind,strsplit(as.character(data[,1]),":"))
 header<-paste(id," Mean Coverage: ", aveCov, sep="")
-plot(as.numeric(data_pos[,2]),log10(data[,2]),main=header,ylab="Log10(cov)",xlab="Pos",ylim=c(0,7),pch=20)
+#plot log coverage against base position
+plot(as.numeric(data_pos[,2]),log10(data[,2]),main=header,ylab="Log10(cov)",xlab="Pos",ylim=c(0,7),pch=20) 
 
-vcf<-read.table(file=args[4],sep="\t",header=FALSE)
+vcf<-read.table(file=args[4],sep="\t",header=FALSE) #vcf file
 vcf_filter<-vcf[which(vcf[,6]>1000),]
 vcf_data<-do.call(rbind,strsplit(as.character(vcf_filter[,8]),";"))
 vcf_freq<-do.call(rbind,strsplit(as.character(vcf_data[,2]),"="))
 vcf_dp<-do.call(rbind,strsplit(as.character(vcf_data[,1]),"="))
+#plot SNP frequency against base position
 points(vcf_filter[,2],as.numeric(vcf_freq[,2])*max(log10(data[,2])),col="red",pch=20)
 dev.off()
